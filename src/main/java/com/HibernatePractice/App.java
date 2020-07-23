efa3f8949738db9ed33da2bd10b00f279944c84c
junaidansari6311 <junaidansari6311@gmail.com>
2020-07-23 16:07:55 +0530

    ADDED : Query to update student details

--- src/main/java/com/HibernatePractice/App.java
+++ src/main/java/com/HibernatePractice/App.java
@@ -1,9 +1,34 @@
 package com.HibernatePractice;
 
-public class App 
+import com.HibernatePractice.model.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.Query;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world");
+        Configuration  configuration = new Configuration();
+        configuration.configure();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        /*String insertStudent = "insert into Student (Junaid,BE,101,9874561230)"+"(select name, degree, roll, phone from Student)";
+        Query query = session.createQuery(insertStudent);
+        int result = query.executeUpdate();*/
+
+        String hql = "update Student set degree = :degreeName " + "where id = :studentId";
+        Query query = session.createQuery(hql);
+        query.setParameter("degreeName", "BSC");
+        query.setParameter("studentId", 3);
+        query.executeUpdate();
+
+        transaction.commit();
+        session.close();
     }
 }
